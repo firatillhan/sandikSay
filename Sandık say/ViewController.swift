15f65c5cd3dfcb71562cf22bce8ef6c3fc79c0d1
Fırat ilhan <firatilhan008@gmail.com>
2023-05-27 23:40:55 +0300

    Initial Commit

---
+++ Sandık say/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sandık say
+//
+//  Created by Fırat İlhan on 27.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
