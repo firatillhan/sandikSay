515fcbfd313ab6bfeadb3961475273ed434c49a8
Fırat ilhan <firatilhan008@gmail.com>
2023-05-28 01:44:46 +0300

    I am ready with my little app

--- Sandık say/ViewController.swift
+++ Sandık say/ViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var sayacRTE: UILabel!
+    @IBOutlet weak var sayacKK: UILabel!
+    @IBOutlet weak var yuzdeRTE: UILabel!
+    var hesapla : Double = 0
+    @IBOutlet weak var yuzdeKK: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+ 
+    }
+    
+    
+    
+    
+    @IBAction func stepperRTE(_ sender: UIStepper) {
+        sayacRTE.text = String(sender.value)
     }
 
+    
+    
+    
+    @IBAction func stepperKK(_ sender: UIStepper) {
+        sayacKK.text = String(sender.value)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func hesapla(_ sender: Any) {
+       
+  
+        
+        if let sayi1 = Double(sayacRTE.text!), let sayi2 = Double(sayacKK.text!) {
+           
+            hesapla = sayi1 * 100 / (sayi1 + sayi2)
+            
+            yuzdeRTE.text = String(format: "%.2f", hesapla)
+            yuzdeKK.text = String(format: "%.2f", 100 - (hesapla))
+        }
+        
+        }
+     
+    }
+    
 
-}
-
+   
+        
